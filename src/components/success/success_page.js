84605a81fc17a7c129c2006187221a53e18880af
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2024-12-02 17:44:03 +0200

    Add files via upload

---
+++ src/components/success/success_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../navigation/navigation.js";
+import Footer from "../footer/footer.js";
+import Success from "../success/success.js";
+
+function SuccessPage() {
+    return(<div>
+        <Navigation />
+        <Success />
+        <Footer />
+    </div>)
+}
+
+export default SuccessPage;
